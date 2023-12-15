d1f432762e95699070619f5b615fdf5c73257fc4
narasimha-1511 <s.narasimha.2005@gmail.com>
2023-12-15 18:14:17 +0530

    bard api setup

---
+++ bard/TextToText.js
@@ -0,0 +1,24 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+// ...
+
+const model = genAI.getGenerativeModel({ model: process.env.MODEL_NAME });
+
+async function TextToText(prompt) {
+  // For text-only input, use the gemini-pro model
+
+  //   const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  //   console.log(text);
+  return text;
+}
+
+// ...
+module.exports = TextToText;
