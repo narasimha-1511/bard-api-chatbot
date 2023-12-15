65990af32acce1d598ddad8b02d153ae3b94a06c
narasimha-1511 <s.narasimha.2005@gmail.com>
2023-12-15 18:15:39 +0530

    completed the setup

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+require("dotenv").config();
 
-const port = process.env.PORT || 3000; // Set a default value for port if not defined
+const port = process.env.PORT; // Set a default value for port if not defined
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html',{root: __dirname + '/public'});
+app.get("/", (req, res) => {
+  console.log("GET request received");
+  res.sendFile("index.html", { root: __dirname + "/public" });
 });
+
+app.use(express.json());
+
+app.use("/bard/getPrompt", require("./routes/route.js"));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
